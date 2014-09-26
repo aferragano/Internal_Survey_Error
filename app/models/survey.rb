d07b6d25f97eaab89826dfdeb03958db8ada8b45
Faraaz Nishtar <faraaznishtar@gmail.com>
2014-09-26 16:00:42 -0400

    create questions model

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -1,7 +1,7 @@
 class Survey < ActiveRecord::Base
-  validates :title, presence: true
-  # Remember to create a migration!
   has_many :questions
   belongs_to :user
   
+  validates :title, presence: true
 end
+  
