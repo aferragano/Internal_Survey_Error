768aef8870da1e8c6a92d6d61a150d7cafad7f78
Anthony F <aferragano@gmail.com>
2014-09-26 14:40:41 -0400

    add form. css

---
+++ app/controllers/login.rb
@@ -0,0 +1,23 @@
+get 'login' do 
+	erb :'login'
+end
+
+post 'login' do 
+	user = User.authenticate(params[:user])
+	unless user.nil?
+		session[:user_id] = user.id 
+		redirect '/'
+	end 
+	redirect 'login'
+end
+
+get '/logout' do 
+	session.clear
+	redirect '/'
+end
+
+post '/signup' do 
+	user = User.create(params[:user])
+	session[:user_id] =user.id
+	redirect '/'
+end
