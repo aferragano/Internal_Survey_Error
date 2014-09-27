31104eee37a86b8a665113fd518c09a0f54ead2e
Faraaz Nishtar <faraaznishtar@gmail.com>
2014-09-27 17:16:32 -0400

    login form not routing to broken link

--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,9 @@
 $( document ).ready(function() {
-    $.colorbox({closeButton: false, width: '250', height:'250px', html:'<div class="login"><center><form action="/login" method="POST"><p>LOGIN</p><br><input type="text" name="email" placeholder="email"><br><br><input type="password" name="password" placeholder="password"><br><br><input class="btn btn-primary" type="submit" name="Login"></form></center></div>'});
 
-    $(".login").submit(function(){
+    $.colorbox({closeButton: false, width: '250', height:'250px', html:'<div class="login"><center><form action="/login" method="POST"><p><br>LOGIN</p><br><input type="text" name="email" placeholder="email"><br><br><input type="password" name="password" placeholder="password"><br><br><input class="btn btn-primary" type="submit" name="Login"></form></center></div>'});
+
+    $(".login").submit(function(event){
+    	event.preventDefault();
     	$.colorbox.close();
     });
 
