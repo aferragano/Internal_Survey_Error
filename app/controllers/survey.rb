35d5664fcaafbadfb191ef11e66f6636a883b92d
Anthony F <aferragano@gmail.com>
2014-09-27 12:01:40 -0400

    create new route create

--- app/controllers/survey.rb
+++ app/controllers/survey.rb
@@ -2,15 +2,17 @@
 # survey create get post update diplay?
 
 
-get '/survey/create' do
-	erb :'/create_survey'
+get '/create' do
+	erb :'/create'
 end
-
+# get '/create_survey' do
+#   "Hello World"
+# end
 
 post '/survey/create/' do
 	@survey = Survey.create(title: params[:survey])
 	# question = @survey.questions.create(question: params[:question])
-	option = question.options.create(option: params[:option])
+	# option = question.options.create(option: params[:option])
 end
 
 post '/question/new' do
