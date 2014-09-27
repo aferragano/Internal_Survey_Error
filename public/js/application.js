5daaa63f54c0cc0b542ca2d97568308e1f2fb2cf
Faraaz Nishtar <faraaznishtar@gmail.com>
2014-09-27 00:35:27 -0400

    home page progress

--- public/js/application.js
+++ public/js/application.js
@@ -1,8 +1,8 @@
 $( document ).ready(function() {
-    // $.colorbox({closeButton: false, width: '250', height:'250px', html:'<div class="login"><center><form action="/login" method="POST"><p>LOGIN</p><br><input type="text" name="email" placeholder="email"><br><br><input type="password" name="password" placeholder="password"><br><br><input type="submit" name="Login"></form></center></div>'});
+    $.colorbox({closeButton: false, width: '250', height:'250px', html:'<div class="login"><center><form action="/login" method="POST"><p>LOGIN</p><br><input type="text" name="email" placeholder="email"><br><br><input type="password" name="password" placeholder="password"><br><br><input class="btn btn-primary" type="submit" name="Login"></form></center></div>'});
 
-    // $(".login").submit(function(){
-    // 	$.colorbox.close();
-    // });
+    $(".login").submit(function(){
+    	$.colorbox.close();
+    });
 
 });
