f8fcdb0f176b97aafe9869f230ab9c8feae8712b
Anthony F <aferragano@gmail.com>
2014-09-27 13:37:37 -0400

    Functional question. with options. Your mom

--- app/controllers/survey.rb
+++ app/controllers/survey.rb
@@ -7,14 +7,26 @@ get '/create' do
 end
 
 post '/create/' do
-	@survey = Survey.create(title: params[:survey])
-	# question = @survey.questions.create(question: params[:question])
-	# option = question.options.create(option: params[:option])
+	@survey = Survey.create(title: params[:title])
+	@question = @survey.questions.create(question: params[:question])
+	@question.options.create(option: params[:option1])
+  @question.options.create(option: params[:option2])
+  @question.options.create(option: params[:option3])
+  @question.options.create(option: params[:option4])
+
+  redirect :'/'
 end
 
-post '/question/new' do
-	question = @survey.questions.create(question: params[:question])
-end
+# This shit broke errything. revisit later.
+# post '/question/new' do
+# 	# # @question = @survey.questions.create(question: params[:question])
+#  #  @survey.questions.last.option.create(option: params[:option1])
+#  #  @question<< Option.create(option: params[:option1])
+#  #  @question<< Option.create(option: params[:option2])
+#  #  @question<< Option.create(option: params[:option3])
+#  #  @question<< Option.create(option: params[:option4])
+#   redirect :'/'
+# end
 
 
 get '/take' do
